c9b228786583da1312fa654156f62181d31d1880
Sahil kumar <sk2522136@gmail.com>
2026-02-07 00:04:41 +0500

    Update

--- client/src/pages/seller/SellerLayout.jsx
+++ client/src/pages/seller/SellerLayout.jsx
@@ -32,7 +32,7 @@ const Sellerlayout = () => {
         <>
             <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-300 py-3 bg-white ">
                 <Link to='/' >
-                    <img  src={assets.logo} alt="logo" className="cusrsor-pointer w-34 md:w-38"/>
+                    <img  src={assets.logoU} alt="logo" className="cusrsor-pointer w-34 md:w-38"/>
                </Link>
                 <div className="flex items-center gap-5 text-gray-500">
                     <p>Hi! Admin</p>
