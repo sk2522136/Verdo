694059d8b02a07bf5a4b3818582e5ee6924fe6be
Sahil kumar <sk2522136@gmail.com>
2026-02-06 23:32:15 +0500

    Fix MongoDB connection setup

--- server/configs/db.js
+++ server/configs/db.js
@@ -10,20 +10,5 @@ const connectDB =  async () =>{
 
 }
 
-// export default connectDB;
-
-
-// const MONGO_URL = "mongodb://127.0.0.1:27017/cartStack";
-
-
-// const connectDB =  async () =>{
-//     try{
-//         mongoose.connection.on('connected' , ()=>console.log("Database Cocnnected successfully"));
-//         await mongoose.connect(`${MONGO_URL}`)
-//          }catch(error){
-//         console.error(error.message);
-//     }
-
-// }
 
 export default connectDB;
