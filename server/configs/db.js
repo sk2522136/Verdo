f4e447b070e26d1897da36cdac9054a9ef53addd
Sahil kumar <sk2522136@gmail.com>
2026-02-06 23:30:27 +0500

    Update Database Connection

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,29 +1,29 @@
 import mongoose  from "mongoose";
 
-// const connectDB =  async () =>{
-//     try{
-//         mongoose.connection.on('connected' , ()=>console.log("Database Conected successfully"));
-//         await mongoose.connect(`${process.env.MONGODB_URI}/CarStackDB`)
-//     }catch(error){
-//         console.error(error.message);
-//     }
-
-// }
-
-// export default connectDB;
-
-
-const MONGO_URL = "mongodb://127.0.0.1:27017/cartStack";
-
-
 const connectDB =  async () =>{
     try{
-        mongoose.connection.on('connected' , ()=>console.log("Database Cocnnected successfully"));
-        await mongoose.connect(`${MONGO_URL}`)
-         }catch(error){
+        mongoose.connection.on('connected' , ()=>console.log("Database Conected successfully"));
+        await mongoose.connect(`${process.env.MONGODB_URI}/Verdo`)
+    }catch(error){
         console.error(error.message);
     }
 
 }
+
+// export default connectDB;
+
+
+// const MONGO_URL = "mongodb://127.0.0.1:27017/cartStack";
+
+
+// const connectDB =  async () =>{
+//     try{
+//         mongoose.connection.on('connected' , ()=>console.log("Database Cocnnected successfully"));
+//         await mongoose.connect(`${MONGO_URL}`)
+//          }catch(error){
+//         console.error(error.message);
+//     }
+
+// }
 
 export default connectDB;
