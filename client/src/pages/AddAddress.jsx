1ac654231293894b55fa2837b491873b1e8cc1b6
Sahil Kumar <sk2522136@gmail.com>
2026-02-08 02:27:36 +0500

    update

--- client/src/pages/AddAddress.jsx
+++ client/src/pages/AddAddress.jsx
@@ -48,7 +48,7 @@ const AddAddress = () => {
         e.preventDefault();
 
         try {
-            const {data} = await axios.post('/api/address/add' , { userId:user.id, address})
+            const {data} = await axios.post('/api/address/add' , {userId: user?._id, address})
 
             if(data.success){
                 toast.success(data.message);
